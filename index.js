857f60e4666fccea0c7864fe8d6c4f51693f6921
Renardo Williams <rennwill@gmail.com>
2023-03-27 14:41:21 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(array)
+{    
+    function isWin (arr){ return arr.result === "W"}
+    let result =  array.find(isWin)
+    if (result != undefined){ return result.year}    
+}
